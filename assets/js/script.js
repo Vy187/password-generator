6b9c5f8849c9a7e24dc2ed18f71af9e6b9b1b9f3
Vy Nguyen <vy1872@gmail.com>
2022-06-23 18:03:16 -0500

    made it where user has to enter a criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,22 @@ function getCriteria() {
   criteria = [];
   do {
     criteria[0] = window.prompt("How long do you want the password to be from 8-128?");
-    if(isNaN(criteria[0]) || criteria[0] == null){
+    if (isNaN(criteria[0]) || criteria[0] == null) {
       window.alert("Invalid Input");
-    }else if(criteria[0] < 8  || criteria[0] > 128){
+    } else if (criteria[0] < 8 || criteria[0] > 128) {
       window.alert("Input has to be 8-128");
     }
   } while (isNaN(criteria[0]) || criteria[0] == null || criteria[0] < 8 || criteria[0] > 128);
 
-  criteria[1] = window.confirm("Do you want it to include lowercases?");
-  criteria[2] = window.confirm("Do you want it to include uppercases?");
-  criteria[3] = window.confirm("Do you want it to include numbers?");
-  criteria[4] = window.confirm("Do you want it to include special characters?");
+  do {
+    criteria[1] = window.confirm("Do you want it to include lowercases?");
+    criteria[2] = window.confirm("Do you want it to include uppercases?");
+    criteria[3] = window.confirm("Do you want it to include numbers?");
+    criteria[4] = window.confirm("Do you want it to include special characters?");
+    if(criteria[1] == false && criteria[2] == false && criteria[3] == false && criteria[4] == false){
+      window.alert("Please select one criteria");
+    }
+  } while(criteria[1] == false && criteria[2] == false && criteria[3] == false && criteria[4] == false);
 
   return criteria;
 }
